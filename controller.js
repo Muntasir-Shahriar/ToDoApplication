93c7395c9008ff17e9a488dec7a354ee81c7b2e3
Muntasir-Shahriar4422 <munta.shahriar@gmail.com>
2018-07-25 01:28:18 +0600

    Update My Application

--- controller.js
+++ controller.js
@@ -7,34 +7,58 @@ var userId = document.getElementById('roll');
 var userbGroup = document.getElementById('bgroup');
 
 //function checkEmptyInput(fieldname) {
-    //debugger
-    //var isEmpty = false;
+//debugger
+//var isEmpty = false;
 
-    //var value = document.getElementById(fieldname).value;
+//var value = document.getElementById(fieldname).value;
 
-    //if (value === "") {
-    //    alert(`${fieldname} name can not be Empty`);
-    //    isEmpty = true;
-    //}
-
-    //return isEmpty;
+//if (value === "") {
+//    alert(`${fieldname} name can not be Empty`);
+//    isEmpty = true;
 //}
 
+//return isEmpty;
+//}
+
+function checkEmptyInput() {
+    var isEmpty = false;
+    userfName = document.getElementById('fname').value;
+    userlName = document.getElementById('lname').value;
+    userId = document.getElementById('roll').value;
+    userbGroup = document.getElementById('bgroup').value;
+
+    if (userfName === "") {
+        isEmpty = true;
+    }
+    else if (userlName === "") {
+        isEmpty = true;
+    }
+    else if (userId === "") {
+        isEmpty = true;
+    }
+    else if (userbGroup === "") {
+        isEmpty = true;
+    }
+
+    return isEmpty;
+}
+
 function addRow() {
-    //if(!checkEmptyInput()){
-    var table = document.getElementById('myTable');
-    var newRow = table.insertRow(table.length),
-        cell1 = newRow.insertCell(0),
-        cell2 = newRow.insertCell(1),
-        cell3 = newRow.insertCell(2),
-        cell4 = newRow.insertCell(3),
-        cell5 = newRow.insertCell(4),
-        cell6 = newRow.insertCell(5);
+    if (!checkEmptyInput()) {
+        var table = document.getElementById('myTable');
+        var newRow = table.insertRow(table.length),
+            cell1 = newRow.insertCell(0),
+            cell2 = newRow.insertCell(1),
+            cell3 = newRow.insertCell(2),
+            cell4 = newRow.insertCell(3),
+            cell5 = newRow.insertCell(4),
+            cell6 = newRow.insertCell(5);
 
         userfName = document.getElementById('fname').value;
         userlName = document.getElementById('lname').value;
         userId = document.getElementById('roll').value;
         userbGroup = document.getElementById('bgroup').value;
+        document.getElementById("btn2").disabled = true;
 
         cell1.innerHTML = userfName;
         cell2.innerHTML = userlName;
@@ -42,14 +66,51 @@ function addRow() {
         cell4.innerHTML = userbGroup;
         cell5.innerHTML = '<input type="button" value = "Edit" onClick="Javacsript:editRow(this)">';
         cell6.innerHTML = '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
-
         //selectedRowToInput();
-    //}
+
+        document.getElementById('fname').value = '';
+        document.getElementById('lname').value = '';
+        document.getElementById('roll').value = '';
+        document.getElementById('bgroup').value = '';
+    }
 }
 function deleteRow(obj) {
-     
+
     var index = obj.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("table");
-    table.deleteRow(index);   
+    var table = document.getElementById("myTable");
+    table.deleteRow(index);
+}
+var rIndex;
+function editRow(object) {
+    document.getElementById("btn2").disabled = true;
+    debugger;
+    var index1 = object.parentNode.parentNode.cells;
+    rIndex = object.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTable");
+    document.getElementById('fname').value = index1[0].innerHTML;
+    document.getElementById('lname').value = index1[1].innerHTML;
+    document.getElementById('roll').value = index1[2].innerHTML;
+    document.getElementById('bgroup').value = index1[3].innerHTML;
+}
+
+function updateRow() {
+    debugger
+    if (!checkEmptyInput()) {
+        var table = document.getElementById("myTable");
+        var fName = document.getElementById('fname').value;
+        var lName = document.getElementById('lname').value;
+        var Id = document.getElementById('roll').value;
+        var bGroup = document.getElementById('bgroup').value;
+
+        table.rows[rIndex].cells[0].innerHTML = fName;
+        table.rows[rIndex].cells[1].innerHTML = lName;
+        table.rows[rIndex].cells[2].innerHTML = Id;
+        table.rows[rIndex].cells[3].innerHTML = bGroup;
+
+        document.getElementById('fname').value = '';
+        document.getElementById('lname').value = '';
+        document.getElementById('roll').value = '';
+        document.getElementById('bgroup').value = '';
+    }
 }
 
